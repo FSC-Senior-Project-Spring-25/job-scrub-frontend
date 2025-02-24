5d6e2771d276e09b73966f3ae17efed8ee16390b
harvey <harveytseng2@gmail.com>
2025-02-23 21:28:09 -0500

    structures report page

---
+++ src/app/report/page.tsx
@@ -0,0 +1,6 @@
+
+
+export default function ReportPage() {
+    // TODO: Implement report page from job_adder_frontend.html and styles.css
+    // WHEN FINISHED PLEASE DELETE job_adder_frontend.html and styles.css since they will be used in React component
+}
