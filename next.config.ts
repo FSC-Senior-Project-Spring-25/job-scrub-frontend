6eb848bb49fac0bb466164722512fa070ea7ebbc
harvey <harveytseng2@gmail.com>
2025-05-05 22:32:45 -0400

    removes api routing rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*', // Change this if your backend runs on a different port
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
