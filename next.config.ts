1146dee92a160cb3ba52f30ba934369f62f9ea5e
harvey <harveytseng2@gmail.com>
2025-05-06 18:33:30 -0400

    adds google profile images to next-images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-
-};
+const nextConfig = {
+    images: {
+      domains: [
+        'lh3.googleusercontent.com',    // Google user profile images
+        'firebasestorage.googleapis.com',  // Firebase Storage images
+        'storage.googleapis.com'        // Another Firebase Storage domain
+      ],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+          pathname: '**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'firebasestorage.googleapis.com',
+          pathname: '**',
+        }
+      ]
+    },
+    // Other Next.js config options
+    reactStrictMode: true,
+  };
 
 export default nextConfig;
